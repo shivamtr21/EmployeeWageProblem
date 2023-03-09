08ed622c1e9cb40192bea63a824c6401561cdae1
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-10 00:17:07 +0530

    Total Wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,9 +8,13 @@ namespace WageCaseProblem
 {
     public class EmployeeWage
     {
-        public static void CheckEmployeePresent()
+        public static void TotalWage()
         {
             int Is_Present = 1;
+            int Wage_Per_Hrs = 20;
+            int EmpHrs = 0;
+
+            int Total_Wage = 0;
 
             Random random = new Random();
             int num = random.Next(2);
@@ -18,12 +22,17 @@ namespace WageCaseProblem
             if (num == Is_Present)
             {
                 Console.WriteLine("Employee is present");
+                EmpHrs = 8;
             }
 
             else
             {
                 Console.WriteLine("Employee is Absent");
+                EmpHrs = 0;
             }
+
+            Total_Wage = (EmpHrs * Wage_Per_Hrs);
+            Console.WriteLine(Total_Wage);
         }
     }
 }
