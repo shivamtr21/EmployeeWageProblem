7621b628a5a3f83c838eccab3db141ae354c8bdb
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-10 00:10:43 +0530

    Check Attendence

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeeWage.CheckEmployeePresent();
         }
     } 
    
