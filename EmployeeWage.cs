96986ddbef967a94281707389f3aacadeb2babfc
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-10 00:09:33 +0530

    Check Attendence

---
+++ EmployeeWage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WageCaseProblem
+{
+    public class EmployeeWage
+    {
+        public static void CheckEmployeePresent()
+        {
+            int Is_Present = 1;
+
+            Random random = new Random();
+            int num = random.Next(2);
+
+            if (num == Is_Present)
+            {
+                Console.WriteLine("Employee is present");
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
