e158c7a953633ad1c661b2762187bcc3671d444d
shivamtr21 <shivamtripathi26297@gmail.com>
2023-03-10 13:24:59 +0530

    Wage Calculate for a month

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -8,39 +8,52 @@ namespace WageCaseProblem
 {
     public class EmployeeWage
     {
-        public static void SwitchCase()
+        public static void WageForMonth()
         {
             int EmpHrs = 0;
             int total_Work_Hour = 0;
             int Wage_Per_Hour = 20;
-            Random random = new Random();
-            int num = random.Next(3);
-
             int total_Salary = 0;
 
-            switch (num)
+            int Total_Days = 20;
+
+            for (int day =1; day <= Total_Days; day++)
             {
-                case 0:
-                    Console.WriteLine("Employee is Absent");
-                    EmpHrs = 0;
-                    break;
 
-                case 1:
-                    Console.WriteLine("Employee is present for part time");
-                    EmpHrs = 4;
-                    break;
+                Random random = new Random();
+                int num = random.Next(3);
 
-                case 2:
-                    Console.WriteLine("Employee is present for full time");
-                    EmpHrs = 8;
-                    break;
+
+
+                switch (num)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        EmpHrs = 0;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Employee is present for part time");
+                        EmpHrs = 4;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Employee is present for full time");
+                        EmpHrs = 8;
+                        break;
+                }
+                total_Work_Hour = (total_Work_Hour + EmpHrs);
+
+                total_Salary = (total_Work_Hour * Wage_Per_Hour);
             }
-            total_Work_Hour = (total_Work_Hour + EmpHrs);
 
-            total_Salary = (total_Work_Hour * Wage_Per_Hour);
-            Console.WriteLine(total_Salary);
+            Console.WriteLine("Total working hour in a Month " + total_Work_Hour);
+            
+            Console.WriteLine("Total salary for a month  " + total_Salary);
+
+
 
         }
-        
+
     }
 }
